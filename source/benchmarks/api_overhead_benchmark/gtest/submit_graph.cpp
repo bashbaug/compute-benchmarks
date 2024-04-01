4934f45fe37a4c48e597d9e9a7fd38758571c333
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-04-01 10:35:55 -0700

    remove unused SubmitGraph test arguments

--- source/benchmarks/api_overhead_benchmark/gtest/submit_graph.cpp
+++ source/benchmarks/api_overhead_benchmark/gtest/submit_graph.cpp
@@ -14,19 +14,16 @@
 
 [[maybe_unused]] static const inline RegisterTestCase<SubmitGraph> registerTestCase{};
 
-class SubmitGraphTest : public ::testing::TestWithParam<std::tuple<Api, bool, size_t, size_t, size_t, bool>> {
+class SubmitGraphTest : public ::testing::TestWithParam<std::tuple<Api, bool, size_t, size_t, bool>> {
 };
 
 TEST_P(SubmitGraphTest, Test) {
     SubmitGraphArguments args{};
     args.api = std::get<0>(GetParam());
-    //args.useProfiling = std::get<1>(GetParam());
     args.inOrderQueue = std::get<1>(GetParam());
-    //args.discardEvents = std::get<3>(GetParam());
     args.numKernels = std::get<2>(GetParam());
     args.kernelExecutionTime = std::get<3>(GetParam());
-    args.kernelType = std::get<4>(GetParam());
-    args.measureCompletionTime = std::get<5>(GetParam());
+    args.measureCompletionTime = std::get<4>(GetParam());
     SubmitGraph test;
     test.run(args);
 }
@@ -36,10 +33,7 @@ INSTANTIATE_TEST_SUITE_P(
     SubmitGraphTest,
     ::testing::Combine(
         ::CommonGtestArgs::allApis(),
-        //::testing::Values(false),         // useProfiling
         ::testing::Values(false, true),   // inOrderQueue
-        //::testing::Values(false, true),   // discardEvents
         ::testing::Values(32u),           // numKernels
         ::testing::Values(1u),            // kernelExecutionTime
-        ::testing::Values(0u),            // kernelType
         ::testing::Values(false, true))); // measureCompletionTime
