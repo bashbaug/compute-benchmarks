c9ae919b5c27aa8c9e4a219a1d33c1f5cbb1b477
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-02-21 16:59:47 -0800

    original version unchanged from 2024 IWOCL micros
    Signed-off-by: Ben Ashbaugh <ben.ashbaugh@intel.com>

---
+++ source/benchmarks/api_overhead_benchmark/definitions/submit_graph.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022-2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#pragma once
+
+#include "framework/argument/basic_argument.h"
+#include "framework/test_case/test_case.h"
+
+struct SubmitGraphArguments : TestCaseArgumentContainer {
+    BooleanArgument inOrderQueue;
+    PositiveIntegerArgument numKernels;
+    PositiveIntegerArgument kernelExecutionTime;
+    BooleanArgument measureCompletionTime;
+
+    SubmitGraphArguments()
+        : inOrderQueue(*this, "InOrderQueue", "Create the queue with the in_order property"),
+          numKernels(*this, "NumKernels", "Number of kernels to submit to the queue"),
+          kernelExecutionTime(*this, "KernelExecutionTime", "Approximately how long a single kernel executes, in us"),
+          measureCompletionTime(*this, "MeasureCompletionTime", "Measures time taken to complete the submission (default is to measure only submit calls)") {}
+};
+
+struct SubmitGraph : TestCase<SubmitGraphArguments> {
+    using TestCase<SubmitGraphArguments>::TestCase;
+
+    std::string getTestCaseName() const override {
+        return "SubmitGraph";
+    }
+
+    std::string getHelp() const override {
+        return "measures time spent in submitting a graph to a SYCL (or SYCL-like) queue on CPU.";
+    }
+};
---
+++ source/benchmarks/api_overhead_benchmark/gtest/submit_graph.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022-2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "definitions/submit_graph.h"
+
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/common_gtest_args.h"
+
+#include <gtest/gtest.h>
+
+[[maybe_unused]] static const inline RegisterTestCase<SubmitGraph> registerTestCase{};
+
+class SubmitGraphTest : public ::testing::TestWithParam<std::tuple<Api, bool, size_t, size_t, bool>> {
+};
+
+TEST_P(SubmitGraphTest, Test) {
+    SubmitGraphArguments args{};
+    args.api = std::get<0>(GetParam());
+    args.inOrderQueue = std::get<1>(GetParam());
+    args.numKernels = std::get<2>(GetParam());
+    args.kernelExecutionTime = std::get<3>(GetParam());
+    args.measureCompletionTime = std::get<4>(GetParam());
+    SubmitGraph test;
+    test.run(args);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SubmitGraphTest,
+    SubmitGraphTest,
+    ::testing::Combine(
+        ::CommonGtestArgs::allApis(),
+        ::testing::Values(false, true),   // inOrderQueue
+        ::testing::Values(32u),           // numKernels
+        ::testing::Values(1u),            // kernelExecutionTime
+        ::testing::Values(false, true))); // measureCompletionTime
